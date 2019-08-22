24699fb868039cc8bf4121f9266bb8d9b2ff35ea
JoacoMM <53914664+JoacoMM@users.noreply.github.com>
2019-08-22 12:30:55 -0300

    fusion

---
+++ isasa.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
